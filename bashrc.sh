f603a54d77e3950014c078d6ef5cbce46f9b2d16
remichartier <remipr.chartier@gmail.com>
2021-02-15 03:28:17 +0000

    change .gitignore bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,6 +1,6 @@
 git config --global user.email "remipr.chartier@gmail.com"
 git config --global user.name "remichartier"
-git remote set-url origin "git@github.com:remichartier/https://github.com/remichartier/023_selfDrivingCarND_PIDControllerInCppProject.git"
+git remote set-url origin "git@github.com:remichartier/023_selfDrivingCarND_PIDControllerInCppProject.git"
 apt install openssh-client
 eval "$(ssh-agent -s)"
 ssh-add .ssh/20201224_1219_github_ed25519
